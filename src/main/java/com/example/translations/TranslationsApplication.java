b9224c6719ffc1e5f33478508b85870af271b45d
Vlad Lavrynovych <vladyslav.lavrynovych.ext@dkv-mobility.com>
2024-01-08 16:01:05 +0200

    first commit

---
+++ src/main/java/com/example/translations/TranslationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.translations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslationsApplication.class, args);
+    }
+
+}
